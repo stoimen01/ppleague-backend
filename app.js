24ce56fc5087219ddfc52ec63cc3ee3662e3f2c6
stoimen01 <stoimen01@gmail.com>
2018-12-25 13:50:40 +0000

    Fixed minor issues.

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const logger = require('morgan');
 const express = require('express');
+const uuid = require('uuid').v4;
 
 const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+const BAD_REQUEST = 400;
 
 const players = {
     "123": {
@@ -33,47 +36,69 @@ const games = {
 };
 
 const router = express.Router();
-router.get("/data", (req, res) => res.json({ players, games }));
+router.get("/data", (req, res) => res.json({
+    players: Object.values(players),
+    games:Object.values(games)
+}));
 
-router.post("/addPlayer", function (req, res) {
-
+router.post("/addPlayer", (req, res) => {
     const { name } = req.body;
 
-    if (Object.values(players).find((player) => player.name === name)) {
-        res.sendStatus(404);
+
+    if (!name || Object.values(players).find((player) => player.name === name)) {
+        res.sendStatus(BAD_REQUEST);
         return;
     }
 
-    const id = "12312131";
-
     const newPlayer = {
-        id: id,
+        id: uuid(),
         name: name,
         wins: 0,
         losses: 0
     };
 
-    players[id] = newPlayer;
-
+    players[newPlayer.id] = newPlayer;
     res.json(newPlayer)
 });
 
 router.post("/addGame", (req, res) => {
     const { player1Id, player1Score, player2Id, player2Score } = req.body;
 
-    if (!(player1Id && player1Score && player2Id && player2Score)) return;
+    if (!(player1Id && player1Score && player2Id && player2Score)) {
+        res.sendStatus(BAD_REQUEST);
+        return;
+    }
+
+    const player1 = players[player1Id];
+    const player2 = players[player2Id];
+
+    if (!(player1 && player2) || isNaN(player1Score) || isNaN(player2Score)) {
+        res.sendStatus(BAD_REQUEST);
+        return;
+    }
+
+    if (player1Score === player2Score) {
+        res.sendStatus(BAD_REQUEST);
+        return;
+    } else if (player1Score > player2Score) {
+        player1.wins += 1;
+        player2.losses += 1;
+    } else {
+        player2.wins += 1;
+        player1.losses += 1;
+    }
 
     const newGame = {
-        id: "wow",
-        timestamp: 10,
+        id: uuid(),
+        timestamp: Date.now(),
         player1Id,
         player1Score,
         player2Id,
         player2Score
     };
 
-    games.push(newGame);
-    res.json(newGame)
+    games[newGame.id] = newGame;
+    res.json({newGame, player1, player2})
 });
 
 app.use('/', router);
